b97ae3e8b64680ca69a475f52218832fc61bd40b
Khyati Baria <khyatibaria55@gmail.com>
2022-02-12 20:25:33 +0000

    Added docs.html

---
+++ index.js
@@ -0,0 +1,16 @@
+let head = document.querySelector('.hero-subhead');
+
+let i = 0;
+let speed = 50;
+let text = 'Build Clean, Modern UI Interfaces';
+
+window.addEventListener('load', typeWriter);
+window.addEventListener('scroll', typeWriter);
+
+const typeWriter=(e)=> {
+  if (i < text.length) {
+    head.innerHTML += text.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
