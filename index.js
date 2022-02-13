d73fe622952de5ba0b7972ea48636d78c0ee8d73
Khyati Baria <khyatibaria55@gmail.com>
2022-02-13 13:33:25 +0000

    refactor: index.js

--- index.js
+++ index.js
@@ -5,9 +5,8 @@ let speed = 50;
 let text = 'Build Clean, Modern UI Interfaces';
 
 window.addEventListener('load', typeWriter);
-window.addEventListener('scroll', typeWriter);
 
-const typeWriter=(e)=> {
+function typeWriter() {
   if (i < text.length) {
     head.innerHTML += text.charAt(i);
     i++;
