431f5170024a524602ea22b1d12a3b34b5339fff
Khyati Baria <khyatibaria55@gmail.com>
2022-02-19 11:22:11 +0000

    fix: alert.js

--- Components/alert/alert.js
+++ Components/alert/alert.js
@@ -1,5 +1,5 @@
 const alert = document.querySelectorAll('.alert-closeble');
-const alertCloseBtn = document.querySelectorAll('.close');
+const alertCloseBtn = document.querySelectorAll('.alert-close');
 for (let i = 0; i <= alert.length; i++) {
   alertCloseBtn[i].addEventListener('click', () => {
     alert[i].style.display = 'none';
