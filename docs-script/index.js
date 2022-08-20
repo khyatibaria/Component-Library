46f610ca3809d3add5347985b1bd5c130ccc41e7
Khyati Baria <67405584+khyatibaria@users.noreply.github.com>
2022-08-20 21:47:56 +0530

    updated script

--- docs-script/index.js
+++ docs-script/index.js
@@ -1,10 +1,12 @@
-let head = document.querySelector('.hero-subhead');
+let head = document.querySelector(".hero-subhead");
+const menu = document.querySelector("#menuBtn");
+const sideBar = document.querySelector(".sidebar");
 
 let i = 0;
 let speed = 50;
-let text = 'Build Clean, Modern UI Interfaces';
+let text = "Build Clean, Modern UI Interfaces";
 
-window.addEventListener('load', typeWriter);
+window.addEventListener("load", typeWriter);
 
 function typeWriter() {
   if (i < text.length) {
@@ -13,3 +15,10 @@ function typeWriter() {
     setTimeout(typeWriter, speed);
   }
 }
+
+const openSideBar = () => {
+  console.log("hehehe");
+  sideBar.classList.toggle("visibility");
+};
+
+menu.addEventListener("click", openSideBar);
